747d526f510d8f17afac7e0a935c53bc0a418523
GargGirish <91026273+GargGirish@users.noreply.github.com>
2021-09-19 23:00:36 +0530

    Add files via upload

---
+++ Verbose.cs
@@ -0,0 +1,87 @@
+﻿using System.Linq;
+
+namespace CoreDevelopers.DataTypes
+{
+    public class Verbose<T> where T : System.IComparable<T>, System.IEquatable<T>
+    {
+        private string[] acOperators = { "+", "-", "/", "*" };
+        private T _Value;
+        private string _Expression;
+
+        public T Value
+        {
+            get { return _Value; }
+        }
+        public string Expression
+        {
+            get { return _Expression; }
+        }
+        public Verbose()
+        {
+            _Expression = "";
+        }
+
+        public static implicit operator Verbose<T>(T value)
+        {
+            return new Verbose<T> { _Value = value, _Expression = value.ToString() };
+        }
+
+        public static implicit operator T(Verbose<T> value)
+        {
+            return value._Value;
+        }
+        public static Verbose<T> operator +(Verbose<T> a, Verbose<T> b)
+        {
+            dynamic v1 = a.Embrace("+")._Value;
+            dynamic v2 = b.Embrace("+")._Value;
+            return new Verbose<T>()
+            {
+                _Value = v1 + v2,
+                _Expression = $"{a._Expression} + {b._Expression}"
+            };
+        }
+        public static Verbose<T> operator -(Verbose<T> a, Verbose<T> b)
+        {
+            dynamic v1 = a.Embrace("-")._Value;
+            dynamic v2 = b.Embrace("-")._Value;
+            return new Verbose<T>()
+            {
+                _Value = v1 - v2,
+                _Expression = $"{a._Expression} - {b._Expression}"
+            };
+        }
+        public static Verbose<T> operator /(Verbose<T> a, Verbose<T> b)
+        {
+            dynamic v1 = a.Embrace("/")._Value;
+            dynamic v2 = b.Embrace("/")._Value;
+            return new Verbose<T>()
+            {
+                _Value = v1 / v2,
+                _Expression = $"{a._Expression} / {b._Expression}"
+            };
+        }
+        public static Verbose<T> operator *(Verbose<T> a, Verbose<T> b)
+        {
+            dynamic v1 = a.Embrace("*")._Value;
+            dynamic v2 = b.Embrace("*")._Value;
+            return new Verbose<T>()
+            {
+                _Value = v1 * v2,
+                _Expression = $"{a._Expression} * {b._Expression}"
+            };
+        }
+        private Verbose<T> Embrace(string sOperator)
+        {
+            if (acOperators.Where(x => x != sOperator).Any(x => _Expression.Contains(x)))
+            {
+                _Expression = $"({_Expression})";
+            }
+            return this;
+        }
+
+        public override string ToString()
+        {
+            return this._Value.ToString();
+        }
+    }
+}
